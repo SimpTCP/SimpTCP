890007aa9ff2a7c6862e1bad650c4ff5944c3926
tlk <jeremieboutoille@gmail.com>
2014-05-24 15:33:47 +0200

    Correct client.c indentation

--- src/client.c
+++ src/client.c
@@ -24,55 +24,70 @@
 
 void error(char *msg)
 {
-  perror(msg);
-  exit(0);
+    perror(msg);
+    exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-  int sockfd;
-  int portno, n;
-  struct sockaddr_in serv_addr;
-  struct hostent *server;
+    int sockfd;
+    int portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256];
 
-  char buffer[256];
-    
-  if (argc < 3) {
-    fprintf(stderr,"usage %s server_hostname server_port\n", argv[0]);
-    exit(0);
-  }
-  /* launch simptcp protocol entity */
-  start_simptcp(DEFAULT_LOCAL_UDP_PORT);
+    if (argc < 3) {
+        fprintf(stderr,"usage %s server_hostname server_port\n", argv[0]);
+        return 1;
+    }
 
-  /* create a simptcp/IPv4 socket */
-  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_SIMPTCP);
-  if (sockfd < 0) 
-    error("ERROR opening socket\n");
-  /* create and fill the server's socket address */
-  portno = atoi(argv[2]);
-  server = gethostbyname(argv[1]);
-  if (server == NULL) {
-    fprintf(stderr,"ERROR, no such host\n");
-    exit(0);
-  }
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,
-	server->h_length);
-  serv_addr.sin_port = htons(portno);
-  
-  /* connect to the server */
-  if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-    error("ERROR connecting");
+    /* launch simptcp protocol entity */
+    if(start_simptcp(DEFAULT_LOCAL_UDP_PORT))
+    {
+        fprintf(stderr, "Unable to launch simptcp_entity\n");
+        return 1;
+    }
 
-  /* read from stdin the message to send */
-  printf("Please enter the message: ");  
-  bzero(buffer,256);
-  fgets(buffer,255,stdin);
-  /* send the message */
-  n = write(sockfd,buffer,strlen(buffer)-1);
-  if (n < 0) 
-    error("ERROR writing to socket");
-  close(sockfd);
-  return 0;
+    /* create a simptcp/IPv4 socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_SIMPTCP);
+    if (sockfd < 0)
+    {
+        error("ERROR opening socket\n");
+        return 1;
+    }
+        
+    /* create and fill the server's socket address */
+    portno = atoi(argv[2]);
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        return 1;
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    /* connect to the server */
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    {
+        error("ERROR connecting");
+        return 1;
+    }
+
+    /* read from stdin the message to send */
+    printf("Please enter the message: ");  
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    /* send the message */
+    n = write(sockfd, buffer, strlen(buffer)-1);
+    if (n < 0)
+    {
+        error("ERROR writing to socket");
+        return 1;
+    }
+
+    close(sockfd);
+    return 0;
 }
